7f131291b8bf17f821ab4e0e57c25b6c9285ba41
DZHANBOLOT <dzhanbolot@Noutbuk-DZHANBOLOT.local>
2025-07-27 22:28:03 +0600

    commit from 27 july

--- myproject/hello/views.py
+++ myproject/hello/views.py
@@ -1,6 +1,17 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import *
+from .forms import TaskForm
 
 def home(request):
     tasks = Task.objects.all()
-    return render(request, 'hello/index.html', {'tasks':tasks})
+
+    if request.method == 'POST':
+        form = TaskForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('/')
+    else:
+        form = TaskForm()
+    return render(request, 'hello/index.html',
+                  {'tasks':tasks,
+                  'form':form})
